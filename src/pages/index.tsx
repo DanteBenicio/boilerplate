8d27f740c25e814410a996e2587e8185f3a0e6bb
DanteBenicio <dantecosta79@gmail.com>
2022-02-05 21:21:16 -0300

    teste

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+
+type HomeProps = {
+  title: string
+}
+
+const bla = 'Hello';
+
+const Home: NextPage<HomeProps> = ({ title }) => (
+  <>
+    <h1>Hello World</h1>
+    <p>{title}</p>
+  </>
+);
+
+export default Home;
