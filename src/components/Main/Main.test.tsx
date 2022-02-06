35754b60be0acf6b955f9109d898737934c5dca4
DanteBenicio <dantecosta79@gmail.com>
2022-02-06 11:37:12 -0300

    Add Main test

---
+++ src/components/Main/Main.test.tsx
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import Main from '.';
+
+describe('<Main />', () => {
+  it('should render heading', () => {
+    render(<Main />);
+
+    expect(screen.getByRole('heading', { name: /main/i })).toBeInTheDocument();
+  });
+
+  it('should take a snapshot from Main component', () => {
+    const { container } = render(<Main />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
