c0284a873b530195112b0d46920ae414471cb15f
DanteBenicio <dantecosta79@gmail.com>
2022-02-05 22:07:28 -0300

    teste 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,13 @@ type HomeProps = {
   title: string
 }
 
-const bla = 'Hello';
-
-const Home: NextPage<HomeProps> = ({ title }) => (
-  <>
-    <h1>Hello World</h1>
-    <p>{title}</p>
-  </>
-);
+const Home: NextPage<HomeProps> = ({ title }) => {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>{title}</p>
+    </>
+  );
+};
 
 export default Home;
