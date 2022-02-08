dbc0c4470955ad413116b0d349dc332db9988465
DanteBenicio <dantecosta79@gmail.com>
2022-02-08 13:51:20 -0300

    :mag: add meta tags and links
    add meta tags and links

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,10 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
+        <title>Boilerplate</title>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/img/logo.svg" />
+        <meta name="theme-color" content="#06092b" />
         <meta name="description" content="A simple project starter to work with Typescript, React, NextJS and Styled Components" />
       </Head>
       <GlobalStyles />
