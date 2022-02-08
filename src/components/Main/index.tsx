21b62f23422ed030145a37d348c0aeec17f841c7
DanteBenicio <dantecosta79@gmail.com>
2022-02-08 13:48:03 -0300

    :fire: removed test components
    removed test components

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -7,17 +7,6 @@ import {
 
 export default function Main() {
   return (
-    <Container>
-      <Wrapper>
-        <Logo src="./logo.svg" />
-        <Subtitle>
-          <h2>React Avançado - Boilerplate</h2>
-          <p>Typescript, React, NextJS e Styled Components</p>
-        </Subtitle>
-        <ImageWrapper>
-          <Image src="/hero-illustration.svg" width="300px" height="300px" objectFit="contain" />
-        </ImageWrapper>
-      </Wrapper>
-    </Container>
+    <h1>Hello World</h1>
   );
 }
