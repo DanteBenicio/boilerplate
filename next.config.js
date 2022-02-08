3ea8551887358cb4b0bb62ae0b1b666f99756789
DanteBenicio <dantecosta79@gmail.com>
2022-02-08 10:23:12 -0300

    :wheelchair: add next-pwa
    add pwa in next-application

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa');
+
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   reactStrictMode: true,
 }
